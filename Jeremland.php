3f22753a00fc26c949f92922ef739b926db03445
Jeremvill <97048598+Jeremvill@users.noreply.github.com>
2022-01-03 18:33:47 +0100

    Add files via upload

---
+++ Jeremland.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jeremland</title>
+</head>
+<body>
+	<h1>Jeremland</h1>
+	<dr/> 
+	<p>IP:Jeremland.aternos.me</p>
+	<h1>Cette Interface est en developpement</h1>
+</body>
+</html>
